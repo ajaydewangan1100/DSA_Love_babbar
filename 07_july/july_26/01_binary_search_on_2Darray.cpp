fc521fd8fe5018be6ef66e87ac2b743bde404fa0
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-07-26 16:09:43 +0530

    binary search on 2D array

---
+++ 07_july/july_26/01_binary_search_on_2Darray.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int start = 0;
+    int end = row*col - 1;
+
+    int mid = start + (end - start / 2);
+
+    while(start <= end) {
+        int element = matrix[mid / col][mid % col];
+
+        if(element == target) {
+            return 1;
+        }else if(element < target) {
+            start = mid + 1;
+        }else {
+            end = mid - 1;
+        }
+        mid = start + (end - start / 2);
+    }
+    return 0;
+}
+
+
+int main () {
+    vector<vector<int>> matrix1 = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+
+    cout<<searchMatrix(matrix1, 16)<<endl;
+    cout<<searchMatrix(matrix1, 12)<<endl;
+
+
+
+
+return 0;
+}
